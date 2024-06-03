295a8a0edd6342544a4974bec45cfd9f6677cd16
teris93 <ira.ternovaya.93@mail.ru>
2024-06-03 22:05:38 +0300

    Create  utils.py

---
+++ utils.py
@@ -0,0 +1,64 @@
+import requests  # вызов библиотеки requests для получения данных с сайта
+import consts  # вызов констант такой как keys, описанный в файле main
+import json  # вызов библиотеки json для преобразования данных
+import decimal  # вызов decimal для корректного расчета
+"""
+В случае, если есть необходимость выводить цену 1 доллара в биткоинах, ответ бы был слишком маленьким
+поэтому я использовал библиотеку decimal для корректного расчета данных
+"""
+
+
+class APIException(Exception):  # собственный класс исключений, является дочерним общего класса Исключений
+    ...
+
+
+class CryptoConverter:  # создание класса с исключениями
+    def __init__(self):
+        ...
+
+    @staticmethod
+    def exceptions(quote: str, base: str, amount: str):  # входные данные передаются в типе данных str
+        if quote == base:  # если введенные типы валют одинаковы, срабатывает исключения одинаковых типов
+            raise APIException(f'Невозможно перевести валюты одинакового типа: Тип введеной валюты - {base}.')
+
+        try:
+            base_ticker = consts.keys[base]
+        except KeyError:  # вызывание исключения KeyError если ключа в файле consts.keys нет, ошибка в названии ключа
+            raise APIException(f'Нет информации про данный тип валют: Тип валюты - {base}.')
+
+        try:
+            quote_ticker = consts.keys[quote]
+        except KeyError:  # вызывание исключения KeyError если ключа в файле consts.keys нет, ошибка в названии ключа
+            raise APIException(f'Нет информации про данный тип валют: Тип валюты - {quote}.')
+
+        try:
+            amount = float(amount)
+        except ValueError:  # вызывание исключения ValueError если количество валюты не относятся к вещественному типу
+            raise APIException(f'Не могу перевести количество переводимой валюты, неверно указано число.')
+
+
+class Price:  # создание класса Price для вывода итогового значения
+    def __init__(self):
+        ...
+
+    @staticmethod
+    def get_price(quote, base, amount):  # передача в get_price трех значений отвечающих за ввод пользователя
+        base_ticker, quote_ticker = consts.keys[base], consts.keys[quote]
+        request = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        # получение данных из сайта creptocompare
+        total = str(round(decimal.Decimal(json.loads(request.content)[base_ticker] * float(amount)), 8))[::-1]
+        # тут не много перемудрил в плане информации, что тут делал,
+        # 1 получал информацию из сайта в виде числа
+        # 2 умножал полученое число на введенное пользователем число amount
+        # 3 Переводил полученный результат в Decimal формат, что бы избавиться от записей подобного типа 2.643e-05
+        # 4 округлял полученное число до 8 знаков после целого значения
+        # 5 перевел полученное число в str формат и перевернул его, что бы начать убирать нули в случае
+        # если число равно такому формату 0.00043200
+
+        for digit in total:  # убирание нулей в случае указанном выше, так же в случае если число является целым
+            # оно переводится во float а значит надо убрать и точку после 0, тем самым число окргулится, если
+            # вещественной части нет
+            if digit == '0' or digit == '.':
+                total = total.replace(digit, '', 1)
+            else:
+                return total[::-1]
