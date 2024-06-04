8aee2da16c961aad6a42d5b4739ab5af5d9b547c
teris93 <ira.ternovaya.93@mail.ru>
2024-06-04 19:52:44 +0300

    Add files via upload

--- consts.py
+++ consts.py
@@ -1,4 +1,2 @@
-TOKEN = '6438654294:AAFSFdQ1ugR7ljzST9UBee4KSQfAcEY3j0c'
-keys = {'биткоин': 'BTC',
-        'эфириум': 'ETH',
-        'доллар': 'USD'}
+TOKEN = "6438654294:AAFSFdQ1ugR7ljzST9UBee4KSQfAcEY3j0c"
+keys = {"биткоин": "BTC", "эфириум": "ETH", "доллар": "USD"}
